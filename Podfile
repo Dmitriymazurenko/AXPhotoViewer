e6d772e7b0823363bd82371af88cf1c05030474f
Dmitry Mazurenko <dmitri@joinhomebase.com>
2018-04-16 18:17:35 +0300

    Trying to add carthage support

---
+++ Podfile
@@ -0,0 +1,15 @@
+platform :ios, '9.0'
+
+target 'AXPhotoViewer' do
+
+  use_frameworks!
+
+  pod 'Reveal-SDK'
+
+  # Loading spinner
+  pod 'DRPLoadingSpinner'
+
+  # AXPhotoViewer
+  pod 'AXPhotoViewer', :path => 'AXPhotoViewer.podspec'
+
+end
---
+++ Supporting Files/AXPhotoLibrary.h
@@ -0,0 +1,19 @@
+//
+//  AXPhotoLibrary.h
+//  AXPhotoLibrary
+//
+//  Created by Dmitry Mazurenko on 4/16/18.
+//  Copyright © 2018 Alex Hill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AXPhotoLibrary.
+FOUNDATION_EXPORT double AXPhotoLibraryVersionNumber;
+
+//! Project version string for AXPhotoLibrary.
+FOUNDATION_EXPORT const unsigned char AXPhotoLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AXPhotoLibrary/PublicHeader.h>
+
+
